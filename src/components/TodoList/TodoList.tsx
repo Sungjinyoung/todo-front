afd65ffe5ce4c811bac90f22a6440940a9206110
Sungjinyoung <sjy02161@naver.com>
2022-01-08 03:01:18 +0900

    todoAPI 호출 모듈 구현

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Box } from '@mui/material'
-import TodoItem from '../TodoItem/TodoItem'
-
-
+import React, { useEffect, useState } from "react"
+import { Box } from "@mui/material"
+import TodoItem from "../TodoItem/TodoItem"
+import { getTodoList } from "../../lib/api"
+import { AxiosError, AxiosResponse } from "axios"
 
 const TodoList = () => {
-	return (
-		<div className='todo-list-container'>
-			<Box sx={{ width: 300, height: 200, border: '1px solid gray' }}>
-				<TodoItem 
-					title='오늘 할일' 
-					description='집에서 과제를 해야해요' 
-					deadline={new Date().getTime()} />
-			</Box>
-		</div>
-	)
+  return (
+    <div className="todo-list-container">
+      <Box sx={{ width: 300, height: 200, border: "1px solid gray" }}>
+        <TodoItem
+          title="오늘 할일"
+          description="집에서 과제를 해야해요"
+          deadline={new Date().getTime()}
+        />
+      </Box>
+    </div>
+  )
 }
 
 export default TodoList
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import TodoList from '../../components/TodoList/TodoList'
+import React, { useEffect } from "react"
+import TodoList from "../../components/TodoList/TodoList"
 
-
-const MainPage:React.FC = () => {
-	return (
-		<div className='main-page'>
-			<TodoList></TodoList>
-		</div>
-	)
+const MainPage = () => {
+  useEffect(() => {
+    //
+  }, [])
+  return (
+    <div className="main-page">
+      <TodoList></TodoList>
+    </div>
+  )
 }
 
 export default MainPage
